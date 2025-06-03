ecd966df6f15b402df1b63780b5c7d844676d493
StamperSoftware <StamperSoftware@gmail.com>
2025-06-03 12:30:05 -0400

    Update Athlete.cs
    Made team required
    Got rid of List of User Team

--- Core/Entities/Athlete.cs
+++ Core/Entities/Athlete.cs
@@ -4,13 +4,9 @@ public class Athlete : BaseEntity
 {
     public string FirstName { get; set; } = "";
     public string LastName { get; set; } = "";
-    
     public int TeamId { get; set; }
-    public Team Team { get; set; }
-    
+    public required Team Team { get; set; }
     public Position Position { get; set; }
-    
-    public IList<UserTeam> UserTeam { get; set; }
 }
 
 public enum Position
