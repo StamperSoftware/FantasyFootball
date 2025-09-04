f194d6c68dea3c55c0732697f998b9ac63e587d3
StamperSoftware <StamperSoftware@gmail.com>
2025-09-03 21:22:45 -0400

    Update AccountsController.cs

--- API/Controllers/AccountsController.cs
+++ API/Controllers/AccountsController.cs
@@ -6,7 +6,6 @@ using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace API.Controllers;
 
